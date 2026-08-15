842221edb178e4e63a997ab84fc400edac405352
haproven tech <haproven.tech@gmail.com>
2026-08-16 03:47:44 +0530

    Refactor loadLivePage function for clarity

--- root/assets/js/global.js
+++ root/assets/js/global.js
@@ -11,38 +11,29 @@ document.getElementById("year").textContent = new Date().getFullYear();
 // ============================================================
 
 (function () {
-
     function loadLivePage() {
-
         const pageDisplay =
             document.getElementById("liv-open-page");
-
         if (!pageDisplay) return;
-
         pageDisplay.textContent =
             `You are on: ${document.title} page.`;
-
     }
-
-
     // DOM अभी load हो रहा है
     if (document.readyState === "loading") {
-
         document.addEventListener(
             "DOMContentLoaded",
             loadLivePage
         );
-
     }
-
     // DOM पहले से load हो चुका है
     else {
-
         loadLivePage();
-
     }
-
 })();
+
+
+
+
 
 
 
